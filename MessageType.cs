9cd07cfc7970cf2795234d7f802d035f441f1bc9
lee dong kyoo <dk910320@gmail.com>
2016-07-28 22:39:43 +0900

    Update MessageType.cs

--- MessageType.cs
+++ MessageType.cs
@@ -1,23 +1,22 @@
 static class MessageType
     {
-
         public enum Types : short
         {
 
-            REQUEST_SIGNUP=100,
-            REQEUST_CHECK_SIGNEDUP=110,
-            REQUEST_LOGIN=120,
-            
-            CHAT_MSG=200,
+            REQUEST_USERID_CHECK=100,
+            REQUEST_SIGNUP =110,
 
-            REQUEST_CREATE_ROOM=310,
-            REQUEST_LEAVE_ROOM=320,
-            REQUEST_JOIN_ROOM=330,
-            REQUEST_LIST_ROOM=340,
+            REQUEST_LOGIN=200,
 
-            STATUS_SUCCESS=200,
+            REQUEST_LIST_ROOM = 300,
+            REQUEST_JOIN_ROOM = 310,
+            REQUEST_LEAVE_ROOM = 320,
+            REQUEST_CREATE_ROOM = 330,
+
+            REQUEST_CHATTING = 400,
+
+            STATUS_REQUEST = 100,
+            STATUS_SUCCESS =200,
             STATUS_FAIL=400,
-
         };
-        
     }
