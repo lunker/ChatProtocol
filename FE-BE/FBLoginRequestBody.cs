026db18ba4be874e8cede5407940dcad71f42616
kimsin3003 <kimsin3003@naver.com>
2016-07-29 14:48:34 +0900

    Update FBLoginRequestBody.cs

--- FE-BE/FBLoginRequestBody.cs
+++ FE-BE/FBLoginRequestBody.cs
@@ -3,4 +3,5 @@ using System.Runtime.InteropServices;
 struct LoginRequestBody{
     [MarshalAs(UnmanagedType.ByValArray, SizeConst = 10)] char[] id;
     [MarshalAs(UnmanagedType.ByValArray, SizeConst = 10)] char[] password;
+    bool isDummy;
 }
