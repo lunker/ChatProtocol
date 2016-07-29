356e6752cec382fbce28e61a2b48d71ad6e5e3e1
kimsin3003 <kimsin3003@naver.com>
2016-07-29 13:57:42 +0900

    Update FBLoginRequestBody.cs

--- FE-BE/Login/FBLoginRequestBody.cs
+++ FE-BE/Login/FBLoginRequestBody.cs
@@ -1,4 +1,6 @@
-struct FBLoginRequestBody{
-  string id;
-  string password;
+using System.Runtime.InteropServices;
+
+struct LoginRequestBody{
+    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 10)] char[] id;
+    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 10)] char[] password;
 }
