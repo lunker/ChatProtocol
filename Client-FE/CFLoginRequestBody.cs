f7a8af81f6271e9073d580b696598e46cc96f62c
Simhyejin <hj12334@naver.com>
2016-07-29 15:29:14 +0900

    Update and rename CFLoginRequestBody.cs to CFSignRequestBody.cs

--- Client-FE/CFLoginRequestBody.cs
+++
@@ -1,7 +0,0 @@
-using System.Runtime.InteropServices;
-
-struct SignRequestBody{
-    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 10)] char[] id;
-    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 10)] char[] password;
-    bool isDummy;
-}
