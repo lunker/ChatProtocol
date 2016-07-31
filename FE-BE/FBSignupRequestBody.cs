6629e7f6603e3fcb1a2b70be257fc2776a12751b
kimsin3003 <kimsin3003@naver.com>
2016-07-31 20:23:53 +0900

    Delete FBSignupRequestBody.cs

--- FE-BE/FBSignupRequestBody.cs
+++
@@ -1,7 +0,0 @@
-struct FBSignupRequestBody
-{
-    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 10)]
-    char[] id;
-    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 10)]
-    char[] password;
-}
