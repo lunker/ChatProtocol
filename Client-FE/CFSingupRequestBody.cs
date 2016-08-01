533e1ca930a500b6bf75bf2df350a0d03cd7dabb
Simhyejin <hj12334@naver.com>
2016-08-01 10:11:43 +0900

    Update CFSingupRequestBody.cs

--- Client-FE/CFSingupRequestBody.cs
+++ Client-FE/CFSingupRequestBody.cs
@@ -3,4 +3,5 @@ using System.Runtime.InteropServices;
 struct CFSignupRequestBody{
     [MarshalAs(UnmanagedType.ByValArray, SizeConst = 12)] char[] id;
     [MarshalAs(UnmanagedType.ByValArray, SizeConst = 16)] char[] password;
+    bool isDummy;
 }
