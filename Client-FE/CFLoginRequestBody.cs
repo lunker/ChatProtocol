5d4a7c2b0b92286c0531fe5412e3bdc05854ac36
Simhyejin <hj12334@naver.com>
2016-08-01 10:11:26 +0900

    Update CFLoginRequestBody.cs

--- Client-FE/CFLoginRequestBody.cs
+++ Client-FE/CFLoginRequestBody.cs
@@ -3,5 +3,4 @@ using System.Runtime.InteropServices;
 struct LoginRequestBody{
     [MarshalAs(UnmanagedType.ByValArray, SizeConst = 12)] char[] id;
     [MarshalAs(UnmanagedType.ByValArray, SizeConst = 16)] char[] password;
-    bool isDummy;
 }
