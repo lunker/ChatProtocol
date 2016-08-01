11c8962040036801f05eeb284cf0a0ff85d5a6d6
Simhyejin <hj12334@naver.com>
2016-08-01 10:09:59 +0900

    Create CFSingupRequestBody.cs

---
+++ Client-FE/CFSingupRequestBody.cs
@@ -0,0 +1,6 @@
+using System.Runtime.InteropServices;
+
+struct CFSignupRequestBody{
+    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 12)] char[] id;
+    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 16)] char[] password;
+}
