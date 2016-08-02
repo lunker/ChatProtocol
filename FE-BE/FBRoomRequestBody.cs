011fd81664b35bd0d5be14dfe693fa39e68dcf2c
lee dong kyoo <dk910320@gmail.com>
2016-08-02 15:06:46 +0900

    Update FBRoomRequestBody.cs

--- FE-BE/FBRoomRequestBody.cs
+++ FE-BE/FBRoomRequestBody.cs
@@ -1,5 +1,14 @@
+/*
+    type : 
+    Room_Create = 310,
+    Room_Leave = 320,
+    Room_Join = 330,
+    Room_List = 340,
+    
+*/
+// 채팅방 관련 요청 시 사용되는 추가적인 data를 위한 body
 struct FBRoomRequestBody{
     [MarshalAs(UnmanagedType.ByValArray, SizeConst = 12)]
-    char[] id;
-    int roomNo;
+    char[] id; // user id
+    int roomNo; // chat room number 
 }
