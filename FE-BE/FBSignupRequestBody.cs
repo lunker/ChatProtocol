c5ce54fc2efe35c4fb0ec97f9d75dabe6cfad49f
lee dong kyoo <dk910320@gmail.com>
2016-08-02 15:10:54 +0900

    Update FBSignupRequestBody.cs

--- FE-BE/FBSignupRequestBody.cs
+++ FE-BE/FBSignupRequestBody.cs
@@ -1,7 +1,9 @@
+// type : Signup = 120
+// client의 회원가입을 처리하기 위한 request body
 struct FBSignupRequestBody
 {
     [MarshalAs(UnmanagedType.ByValArray, SizeConst = 12)]
-    char[] id;
+    char[] id; // user id 
     [MarshalAs(UnmanagedType.ByValArray, SizeConst = 16)]
-    char[] password;
+    char[] password; // user password 
 }
