89d75bd16da1f25b7d183e892f60fda3318e8773
lee dong kyoo <dk910320@gmail.com>
2016-08-02 14:57:56 +0900

    Update CBRanking.cs

--- Client-BE/CBRanking.cs
+++ Client-BE/CBRanking.cs
@@ -1,7 +1,8 @@
+// containing single rank information
+// will be used as struct array to provide Ranking information
 public struct CBRanking
 {
     [MarshalAs(UnmanagedType.ByValArray, SizeConst = 12)]
-    char[] id;
-
-    int rank;
+    char[] id; // user id
+    int rank; // user's rnak 
 }
