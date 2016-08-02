67324c6cd1980a91fa2960f7549503931463365f
lee dong kyoo <dk910320@gmail.com>
2016-08-02 15:01:44 +0900

    Update FBChatRequestBody.cs

--- FE-BE/FBChatRequestBody.cs
+++ FE-BE/FBChatRequestBody.cs
@@ -1,6 +1,4 @@
-
-using System.Runtime.InteropServices;
-
+// type : Chat_Count = 410 일때 사용. 
 struct FBChatRequestBody{
-    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 12)] char[] id;
+    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 12)] char[] id;// user id 
 }
