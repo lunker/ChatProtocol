87823fd067c7d40dc806826a2ce25387ce6ee391
lee dong kyoo <dk910320@gmail.com>
2016-08-02 15:03:32 +0900

    Update FBLoginRequestBody.cs

--- FE-BE/FBLoginRequestBody.cs
+++ FE-BE/FBLoginRequestBody.cs
@@ -1,6 +1,7 @@
 using System.Runtime.InteropServices;
-
+// type : Login = 210
+// Client의 로그인 요청을 처리하기 위하여 필요한 body
 struct FBLoginRequestBody{
-    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 12)] char[] id;
-    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 16)] char[] password;
+    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 12)] char[] id; // user id 
+    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 16)] char[] password; // user password
 }
