3b3369aa03317a068ea547942160d8a028181581
lee dong kyoo <dk910320@gmail.com>
2016-08-02 14:56:45 +0900

    Update CBHeader.cs

--- Client-BE/CBHeader.cs
+++ Client-BE/CBHeader.cs
@@ -1,9 +1,10 @@
+// struct for Client ~ BE Header
 [StructLayout(LayoutKind.Sequential, Pack = 2)]
 public struct CBHeader
 {
-    public CBMessageType type;
-    public CBMessageState state;
-    public int length;
+    public CBMessageType type; // message type
+    public CBMessageState state; // message status
+    public int length; // body length 
 
     public int Length
     {
@@ -23,19 +24,19 @@ public struct CBHeader
         set { this.state = value; }
     }
 }
-
+// 
 public enum CBMessageType : short
 {
 
-    Total_Room_Count = 110,
-    FE_User_Status = 210,
-    Chat_Ranking = 310
+    Total_Room_Count = 110, // type for request total room number in application
+    FE_User_Status = 210, // type for request FE' user number
+    Chat_Ranking = 310 // type for request chatting ranking 
     
 };
 
 public enum CBMessageState : short
 {
-    REQUEST = 100,
-    SUCCESS = 200,
-    FAIL = 400
+    Request = 100, // state that this message is 'Request'
+    Success = 200, // state that this message is Response messgae with 'Success'
+    Fail = 400 // state that this message is Response message with 'Fail'
 }
