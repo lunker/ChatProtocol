73fc5e029f33328ff83b7bc5cb8d7e53c14b03b7
lee dong kyoo <dk910320@gmail.com>
2016-08-02 15:05:44 +0900

    Update FBLoginResponseBody.cs

--- FE-BE/FBLoginResponseBody.cs
+++ FE-BE/FBLoginResponseBody.cs
@@ -1,5 +1,6 @@
+// type : Login = 210
+// client의 로그인 요청에 대한 응답 
 struct FBLoginResponseBody{
-  [MarshalAs(UnmanagedType.LPArray, SizeConst = 12)]
-  public char[] id;
-  bool isDummy;
+    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 12)]
+    public char[] id; // user id 
 }
