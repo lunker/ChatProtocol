2baac87bcb61d297ac10adb67b90d942a2f6b07b
lee dong kyoo <dk910320@gmail.com>
2016-08-04 15:39:46 +0900

    Update CBFEUserStatus.cs

--- Client-BE/CBFEUserStatus.cs
+++ Client-BE/CBFEUserStatus.cs
@@ -3,8 +3,11 @@
 // FE Monitoring information will be sent to Client as struct CBFEUserStatus array 
 public struct CBFEUserStatus
 {
-    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 12)]
-    char[] feName; // FE Server Name ex) fe1, fe2, fe3 . . . 
-    int num; // FE' user count
+    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 15)]
+    char[] ip;
+    int port;
+
+    int num;
+
 
 }
