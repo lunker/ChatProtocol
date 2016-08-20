3dd2b4eea32ad7f091384b438eb90190c0c43cb2
kimsin3003 <kimsin3003@naver.com>
2016-08-21 01:04:50 +0900

    Delete CBRanking.cs

--- Client-BE/CBRanking.cs
+++
@@ -1,8 +0,0 @@
-// containing single rank information
-// will be used as struct array to provide Ranking information
-public struct CBRanking
-{
-    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 12)]
-    char[] id; // user id
-    int rank; // user's rnak 
-}
